75195812d777526d7ae5251bf66034e543542358
reesher <reesh197@gmail.com>
2016-01-24 19:41:17 +0100

    added admin styles, fixed tgmpa notification

--- functions.php
+++ functions.php
@@ -153,6 +153,16 @@ require get_template_directory() . '/inc/customizer.php';
  */
 require get_template_directory() . '/inc/jetpack.php';
 /**
+ * Load Admin Style
+ */
+function load_custom_wp_admin_style() {
+        wp_register_style( 'admin_css', get_template_directory_uri() . '/admin.css', false, '1.0.0' );
+        wp_enqueue_style( 'admin_css' );
+}
+add_action( 'admin_enqueue_scripts', 'load_custom_wp_admin_style' );
+
+
+/**
  * Load RequiredPlugins.
  */
  /**
