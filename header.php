28e2641185d5aabdece0ed9e091e6b88a584c5af
Andrej Srna <reesh197@gmail.com>
2016-02-07 12:16:35 +0100

    Upload Logo from theme customizer

--- header.php
+++ header.php
@@ -26,6 +26,9 @@
 
 	<header id="masthead" class="site-header" role="banner">
 		<div class="site-branding">
+			<div class='site-logo'>
+					<a href='<?php echo esc_url( home_url( '/' ) ); ?>' title='<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>' rel='home'><img src='<?php echo esc_url( get_theme_mod( 'themeslug_logo' ) ); ?>' alt='<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>'></a>
+			</div>
 			<?php
 			if ( is_front_page() && is_home() ) : ?>
 				<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
